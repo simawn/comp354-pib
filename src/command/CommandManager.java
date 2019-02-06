3c8c86c4b97410f2617b69382395241c8c273df7
david <david.ian.gray@gmail.com>
2019-02-06 16:19:44 -0500

    Documented command pattern

--- src/command/CommandManager.java
+++ src/command/CommandManager.java
@@ -3,9 +3,12 @@ package command;
 import java.util.ArrayList;
 
 /**
- *
  * Command manager that contains storeAndExecute(cmd) to execute and store commands
+ * Also maintains a history of commands called.
+ * 
+ * @author David Gray
  */
+
 public class CommandManager {
     private ArrayList<Command> history = new ArrayList<>();
     
