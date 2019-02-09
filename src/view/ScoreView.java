1fb1ec1a99728cbee4bac825e91af9dd7a93344f
Anthony Funiciello <37711422+anthroe@users.noreply.github.com>
2019-02-08 23:36:33 -0500

    scoreboard now displays turn

--- src/view/ScoreView.java
+++ src/view/ScoreView.java
@@ -1,27 +1,21 @@
 package view;
 
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.board.CardType;
 import model.board.Subject;
-import javafx.application.Application;
-import javafx.concurrent.Task;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-
-
-
-import javafx.util.Duration;
+import model.board.GameManager;
+import model.player.Player;
+import model.board.Card;
 /**
 * VerboseView logs game play information, in an optionally visible window.
 * 
@@ -29,42 +23,39 @@ import javafx.util.Duration;
 * @date 02/06/19
 */
 public class ScoreView implements Observer {
-    private VBox vbox;
+    //private HBox hbox;
     private Stage stage;
     private Subject subject;
-
+    private Label t = new Label();
+    private Label redScore = new Label();
+    private Label blueScore = new Label();
+    private GameManager game;
+    private CardType winningTeam;
+    private Player player;
+    private Card card;
     /**
      * Create the VerboseView with a Subject s that it will be bound to.
      * @param s 
      */
     public ScoreView(Subject s) {
-          this.subject = s;
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        this.subject = s;
 
-       StackPane root = new StackPane();
-        root.getChildren().add(btn);
- Scene scene = new Scene(root, 300, 250);
-
+        
+        
+        Scene scene = new Scene(t, 300, 250);
         scene.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode() == KeyCode.S) {
                 open();
             }
         });
-
-        
-
+        Group root = new Group();
         stage = new Stage();
         stage.setTitle("Codenames - Score");
-        stage.setScene(scene);
-        
+        root.getChildren().add(t);
+        root.getChildren().add(redScore);
+        root.getChildren().add(blueScore);
+        stage.setScene(new Scene(root, 300, 250));
+   
     }
 
     /**
@@ -72,13 +63,20 @@ public class ScoreView implements Observer {
      * @param arg 
      */
     public void log(String arg) {
-        Text t = new Text(arg);
+
         if (arg.contains("Blue")) {
-            t.setFill(Color.BLUE);
+             t.setTextFill(Color.BLUE);
+           t.setText("Blue's Turn");
         } else if (arg.contains("Red")) {
-            t.setFill(Color.RED);
+             t.setTextFill(Color.RED);
+             t.setText("Red's Turn");
         }
-        vbox.getChildren().add(t);
+        /*if(game.declareWinner(player, card) == CardType.Red){
+            t.setText("Red wins!");
+        }else{
+            t.setText("Blue wins!");
+        }*/
+        //hbox.getChildren().add(t);
     }
 
     /**
