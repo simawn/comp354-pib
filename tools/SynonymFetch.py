81b380f8fcc2d9a6c17618aaa16c998707a1af46
Simon Huang <simon@simonhuang.ca>
2019-02-23 00:11:17 -0500

    Synonym fetching tool working

--- tools/SynonymFetch.py
+++ tools/SynonymFetch.py
@@ -13,51 +13,52 @@ import time
 import requests
 import json
 
-"""
-r = requests.get("https://api.datamuse.com/words?rel_spc=ocean")
-j = json.loads(r.text)
-print(j[0]["word"])
-"""
 # C:\Users\Simon\Documents\Code\comp354-pib\tools\words100_1550871908.txt
 # C:\Users\Simon\Documents\Code\comp354-pib\tools\words10_1550875193.txt
+MAX_SYN = 4
 
-wordList = open(input("File path: "), "r")
-lines = [line.rstrip('\n') for line in wordList]
-output = {}
+def main():
+    wordList = open(input("File path: "), "r")
+    lines = [line.rstrip('\n') for line in wordList]
+    output = {}
 
-for line in lines:
-    output[line] = {} # Actual word
-    output[line]["syn"] = syn_search(line) # Create a "syn" attribute and setting it as an array
-    #start appending
+    for line in lines:
+        print("Working on: " + line)
+        output[line] = {} # Actual word
+        output[line]["syn"] = syn_search(line) # Create a "syn" attribute and setting it as an array
+
+    jsonOutput = json.dumps(output)
+
+    filename = os.path.splitext(os.path.basename(wordList.name))[0] + "_SYN_" + str(int(time.time()))
+    with open(os.path.join(os.path.dirname(__file__),filename + ".json"), 'w') as w:
+        w.write(jsonOutput)
+    print("Done")
 
 def syn_search(word):
     processArray = []
 
-    searchType = "rel_syn" #refer to API docs
+    searchType = "rel_syn" # Refer to API docs
     apiCall = requests.get("https://api.datamuse.com/words?" + searchType + "=" + word)
-    result = json.loads(apiCall.text)
-    # loop through results and reject if:
-    # contains more than 2 words
-    # the actual word is contained in the synonym
-    # if pass, append to array
-    # stop and return array when array length 4. if less than 4:
-    # run extra_search()
+    apiResult = json.loads(apiCall.text)
+    for result in apiResult:
+        if processArray.__len__() is MAX_SYN:
+            break
+        curSyn = result["word"]
+        # Check if current word is not in the clue and the clue is only 1 word
+        if word not in curSyn and len(curSyn.split()) is 1:
+            processArray.append(curSyn)
+
+    # alt search
+    if processArray.__len__() < MAX_SYN:
+        extraApiCall = requests.get("https://api.datamuse.com/words?rel_trg=" + word)
+        extraApiCallResult = json.loads(extraApiCall.text)
+        for extraResult in extraApiCallResult:
+            if processArray.__len__() is MAX_SYN:
+                break
+            curSyn = extraResult["word"]
+            if word not in curSyn and len(curSyn.split()) is 1:
+                processArray.append(curSyn)
+
     return processArray
 
-def extra_search(word):
-    #extra search if there are less than 4 words found
-    #rel_spc
-    #rel_gen
-    #rel_trg
-    pass
-
-"""
-for line in lines:
-    output[line] = {}
-    output[line]["syn"] = []
-    output[line]["syn"].append("syn1")
-    output[line]["syn"].append("syn2")
-    output[line]["syn"].append("syn3")
-
-print(json.dumps(output))
-"""
+main()
