01fe43920c9d6fe2626b4a7434e1c1b98535f493
Max <mpage-sowik@outlook.com>
2019-02-26 10:29:47 -0500

    Added handler classes for start game, end game, and difficulty change

---
+++ src/control/game/GameControls.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control.game;
+
+import javafx.event.EventHandler;
+
+/**
+ *
+ * @author Max Page-Slowik
+ */
+public class GameControls implements EventHandler<ActionEvent>{
+    
+}
