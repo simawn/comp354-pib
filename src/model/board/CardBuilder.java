4f05ccbe3e0ff0f9deb957c7c84cb299fdecd801
Simon Huang <simon@simonhuang.ca>
2019-03-01 18:58:49 -0500

    Modified code so it can process .json files. The provided .json can now be parsed into Words

--- src/model/board/CardBuilder.java
+++ src/model/board/CardBuilder.java
@@ -1,49 +1,86 @@
 package model.board;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+
+/*
+ * The .jar (in /lib folder) will need to be imported:
+ * How to import in Eclipse:
+ * Project > Properties > Java Build Path > Libraries > Add External JARs >
+ * Choose “json-simple-1.1.1.jar” in the /lib folder
+ */
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
 * Extractor is an abstract class for reading lines of a text file doing something
 * with (parsing) them.
 * 
-* @author Rani Rafid
+* @author Rani Rafid, Simon Huang
 * @date 02/06/19
 */
 abstract class Extractor {
     static final int SIZE = 25;
 
     List<String> build(Path Path) throws IOException {
-        if (!Files.exists(Path)) {
+    	
+    	if (!Files.exists(Path)) {
             throw new IOException("Error: Missing " + Path);
         }
 
         List<String> list = Files.readAllLines(Path);
-        Collections.shuffle(list);
+        Collections.shuffle(list); //WARNING: The provided .json is a single line. Shuffle won't work here.
 
         return list;
     }
 
-    abstract Object[] parse() throws IOException;
+    abstract Object[] parse() throws IOException, ParseException;
 }
 
 /**
 * Word class is responsible for reading the codename words from a text file,
 * and returning the first 25 of them.
 * 
-* @author David Gray, Rani Rafid
+* @author David Gray, Rani Rafid, Simon Huang
 * @date 02/06/19
 */
 class Word extends Extractor {
-    private static final Path PATH = Paths.get("resources/words.txt");
+    private static final Path PATH = Paths.get("resources/words100_1550871908_SYN_1550898480.json"); //new file format
 
-    String[] parse() throws IOException {
+    String[] parse() throws IOException, ParseException {
         List<String> temp = build(PATH);
-        return temp.toArray(new String[SIZE]);
+        
+        //Need to convert temp into an actual string. .toString() converts it into an invalid format
+        StringBuilder sb = new StringBuilder();
+        for(String str : temp) {
+        	sb.append(str);
+        }
+        
+        //Start .jsonParse
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(sb.toString()); //Read .json file
+        
+        JSONObject jsonObj = (JSONObject) obj;
+        
+        ArrayList<String> jsonList = new ArrayList<String>();
+        
+        for(Iterator it = jsonObj.keySet().iterator(); it.hasNext();) { //Loop through keys
+        	String key = (String) it.next();
+        	System.out.println(key);
+        	jsonList.add(key);
+        }
+        
+        Collections.shuffle(jsonList); //Needed since it won't shuffle on build. .json file is a single line.
+        
+        return jsonList.toArray(new String[SIZE]);
     }
 
 }
@@ -52,13 +89,13 @@ class Word extends Extractor {
 * KeyCard parses a random line of the text file containing keycards by 
 * mapping them to an array of enum CardType variables.
 * 
-* @author David Gray, Rani Rafid
+* @author David Gray, Rani Rafid, Simon Huang
 * @date 02/06/19
 */
 class KeyCard extends Extractor {
     private static final Path PATH = Paths.get("resources/keyCards.txt");
 
-    CardType[] parse() throws IOException {
+    CardType[] parse() throws IOException, ParseException {
         List<String> list = build(PATH);
 
         String temp = list.remove(0);
@@ -78,7 +115,7 @@ class KeyCard extends Extractor {
 * CardBuilder creates an array of Card objects based on a 
 * random selection of 25 words and a key card.
 * 
-* @author David Gray, Rani Rafid
+* @author David Gray, Rani Rafid, Simon Huang
 * @date 02/06/19
 */
 public class CardBuilder {
@@ -105,7 +142,10 @@ public class CardBuilder {
         } catch (IOException | IllegalArgumentException e) {
             System.err.println("Error in finding path to resources/words.txt | resources/keyCards.txt");
             System.exit(0);
-        }
+        } catch (ParseException e) {
+			System.err.println("Error parsing .json file");
+			System.exit(1);
+		}
 
         return cards;
     }
