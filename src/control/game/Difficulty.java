a95b83c9f0a53aaa8f37c65157c67ed02583f1b6
Max <mpage-sowik@outlook.com>
2019-03-06 17:00:33 -0500

    added preview window instead of menu in order to set dificulty and start

--- src/control/game/Difficulty.java
+++ src/control/game/Difficulty.java
@@ -5,10 +5,38 @@
  */
 package control.game;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.MenuItem;
+
 /**
  *
  * @author Max Page-Slowik
  */
 public class Difficulty {
-    
+
+    private static int difficulty = 0;
+
+    public Difficulty() {
+    }
+
+    public static EventHandler<ActionEvent> setDifficulty() {
+        return new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                MenuItem m = (MenuItem) event.getSource();
+                String s = m.getText();
+                if (s.equals("Easy")) {
+                    difficulty = 0;
+                } else if (s.equals("Medium")) {
+                    difficulty = 1;
+                } else if (s.equals("Hard")) {
+                    difficulty = 2;
+                }
+            }
+        };
+    }
+    public static int getDifficulty(){
+        return difficulty;
+    }
 }
