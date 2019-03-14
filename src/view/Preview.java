d783fa15d93beb5b9f3152f82df1ce905c3fc1ed
mairsarmy32 <46584415+mairsarmy32@users.noreply.github.com>
2019-03-13 21:07:36 -0400

    added comments

--- src/view/Preview.java
+++ src/view/Preview.java
@@ -28,12 +28,16 @@ import model.board.GameManager;
 import model.util.Verbose;
 
 /**
- *
+ * This class sets up a default window template that will be used for our game when it is started or restarted. 
  * @author Max Page-Slowik
  */
 public class Preview {
     private static Stage stage;
 
+    /**
+     * this method creates a default platform that will be used for our game
+     * @param root	a default platform template for our game
+     */
     public static void setUp(Stage root) {
         stage = root;
         VBox vb = new VBox();
@@ -41,8 +45,12 @@ public class Preview {
         vb.setSpacing(5);
         vb.setBackground(new Background(new BackgroundFill(Color.valueOf("877567"), CornerRadii.EMPTY, Insets.EMPTY)));
         Scene scene = new Scene(vb);
+        
+        //creates a label for difficulty
         final Label difficultyLbl = new Label("Difficulty:");
         difficultyLbl.setTextFill(Paint.valueOf("FFFFFF"));
+        
+        //creates a drop down menu where difficulty level will be selected
         final ComboBox difficultyComboBox = new ComboBox();
         difficultyComboBox.getItems().addAll(
                 "Easy",
@@ -53,30 +61,37 @@ public class Preview {
         difficultyComboBox.setOnAction(Difficulty.setDifficulty());
         VBox innerVbCombo = new VBox(difficultyLbl,difficultyComboBox);
         Button startB = new Button("Start");
+        
+        //When start button is pressed, the game begins
         startB.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
                 start_game(root);
             }
         });
+        
+        //adjust the settings of our object
         difficultyComboBox.setPadding(new Insets(5,5,5,5));
         startB.setPadding(new Insets(5,5,5,5));
         ((VBox) scene.getRoot()).getChildren().addAll(innerVbCombo);
         ((VBox) scene.getRoot()).getChildren().addAll(startB);
         
+        //set the title of our platform as well as the height and width of screen.
         root.setTitle("Codenames - Game");
         root.setScene(scene);
         root.setMinHeight(100);
         root.setMinWidth(300);
+        //set the visibility to true and show the window
         root.show();
-        //return scene;
+     
     }
     public static Stage getStage(){
         return stage;
     }
     /**
      * Game model, and game controls are instantiated.
-     * @param root For the instantiated
+     * 
+     * @param root a default platform template for our game
      */
     public static void start_game(Stage root) {
         
@@ -84,15 +99,21 @@ public class Preview {
         Board board = new Board(cards);
         GameManager game = new GameManager(board);
 
+        //assign a view with a Subject that it will be bound to 
         VerboseView view = new VerboseView(Verbose.get());
         Verbose.bind(view);
+        
+        //create a view that will show the scores and attach it to our window
         ScoreView score = new ScoreView(game);
         game.attach(score);
+        
         GameHandler handler = new GameHandler(game, view, score);
         Scene scene = GameScene.build(cards, handler);
         root.setScene(scene);
         root.setResizable(false);
         root.setTitle("Codenames - Game");
-        root.show(); // Shows the final Stage based on the scene
+        
+     // Shows the final Stage based on the scene
+        root.show(); 
     }
 }
