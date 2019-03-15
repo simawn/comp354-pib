6a951c0a8b6af2f0b86ad2f777f37e97bcb046a7
Simon Huang <simon@simonhuang.ca>
2019-03-14 21:03:24 -0400

    Added test to check if all keycards have the correct amount of type

--- tests/model/board/KeyCardTest.java
+++ tests/model/board/KeyCardTest.java
@@ -41,6 +41,26 @@ public class KeyCardTest {
         System.out.println("length of array: " + result.length);
         assertTrue("return an array of 25 key cards", result.length == 25 && result != null);
         
+        //Checking if the amount of card type is accurate
+        int[] amount = {0, 0, 0, 0}; //0: Assassin, 1: Bystanders, 2: Blue, 3: Red
+        for(CardType card : result) {
+        	if(card == card.Assassin) amount[0]++;
+        	if(card == card.Bystander) amount[1]++;
+        	if(card == card.Blue) amount[2]++;
+        	if(card == card.Red) amount[3]++;
+        }
+        
+        assertTrue("The amount of assassin is 1", amount[0] == 1);
+        assertTrue("The amount of bystanders is 7", amount[1] == 7);
+        
+        if(amount[2] == 8) { //If blue is 8, then red is 9
+        	assertTrue("The amount of blue is 8", amount[2] == 8);
+        	assertTrue("The amount of red is 9", amount[3] == 9);
+        } else if (amount[3] == 8) { //If red is 8, then blue is 9
+        	assertTrue("The amount of blue is 8", amount[3] == 8);
+        	assertTrue("The amount of red is 9", amount[2] == 9);
+        } else {
+        	fail("Red and/or blue card amount invalid");
+        }
     }
-    
 }
